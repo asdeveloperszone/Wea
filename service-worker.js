6f00be8e04588dc8b37765f449d7acd1c0cd3359
asdeveloperszone <asdeveloperszone@gmail.com>
2026-02-18 09:40:45 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+// Service Worker for AS-DEV Weather
+const CACHE_NAME = 'asdev-weather-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/favicon-16.png',
+    '/icons/favicon-32.png',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png',
+    '/icons/apple-icon-180.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
